b8bdf1f621e84c57adfa4b2961f892a27ccc0f51
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-21 15:00:41 +0700

    membuat objek dan menampilkan data

--- oop.cpp
+++ oop.cpp
@@ -12,4 +12,11 @@ class Mahasiswa{
             cout<<"NILAI = "<< nilai <<endl;
         }
 };
-int main()
+int main(){
+    Mahasiswa mhs1;
+
+    mhs1.nim = 111;
+    mhs1.nama = "Joko Susilo";
+    mhs1.nilai = 75.5;
+    mhs1.printData();
+}
